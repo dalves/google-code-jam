4eba9eb2767b6bfacc1c28d564456da4acb19e05
David Alves <dalves@fb.com>
2015-04-16 17:29:37 -0700

    My solutions from the 2015 Qualification Round

---
+++ 2015/qual_b_pancakes.py
@@ -0,0 +1,22 @@
+from __future__ import division
+
+T = int(raw_input())
+
+for t in xrange(1, T+1):
+    D = raw_input()
+    arr = map(int, raw_input().split())
+    highest = max(arr)
+
+    res = highest
+
+    for max_stack in xrange(1, highest):
+        splits = 0
+        for i in arr:
+            while i > max_stack:
+                i -= max_stack
+                splits += 1
+        if splits + max_stack < res:
+            print splits, max_stack
+        res = min(res, splits + max_stack)
+
+    print("Case #{}: {}".format(t, res))
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# google-code-jam
-My solutions from various GCJ competitions
+# Google Code Jam
+My solutions from various GCJ competitions. Unless otherwise noted, all
+solutions were written during the competition.
+
